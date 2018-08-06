729d19088f2535d2db3148a3a0564da641a62970
Franz Erlendorf <franz.erlendorf@gmail.com>
2018-08-06 03:59:49 +0200

    Make the background page non-persistent

--- background.js
+++ background.js
@@ -1,12 +1,14 @@
 chrome.contextMenus.create(
     {
+        "id": "define",
         "title": "Define \"%s\"",
         "contexts":["selection"],
-        "onclick": function(info, tab){
-            chrome.tabs.create(
-                {url: "http://www.google.com/search?q=define" + encodeURIComponent(" " + info.selectionText).replace(/%20/g, "+"),
-                index: tab.index + 1,
-                openerTabId: tab.id
-            });
-        }
+});
+
+chrome.contextMenus.onClicked.addListener(function(info, tab){
+    chrome.tabs.create(
+        {url: "http://www.google.com/search?q=define" + encodeURIComponent(" " + info.selectionText).replace(/%20/g, "+"),
+        index: tab.index + 1,
+        openerTabId: tab.id
+    });
 });

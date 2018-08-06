46479e5bef4f6a642003f9737a7d394479a44dec
Franz Erlendorf <franz.erlendorf@gmail.com>
2018-08-06 03:42:30 +0200

    Make the tab open directly to the right of the tab the lookup was started from, instead of at the end of all the window's tabs. Also, make the created tab have set an opener tab for it (affecting what tab gets focus when you close a tab).

--- background.js
+++ background.js
@@ -4,7 +4,9 @@ chrome.contextMenus.create(
         "contexts":["selection"],
         "onclick": function(info, tab){
             chrome.tabs.create(
-                {url: "http://www.google.com/search?q=define" + encodeURIComponent(" " + info.selectionText).replace(/%20/g, "+")
+                {url: "http://www.google.com/search?q=define" + encodeURIComponent(" " + info.selectionText).replace(/%20/g, "+"),
+                index: tab.index + 1,
+                openerTabId: tab.id
             });
         }
 });
